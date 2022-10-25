23022db516cf1f00e555c3fd0936a0d3ed4315a6
gecoerp <36739438+gecoerp@users.noreply.github.com>
2022-10-25 13:48:28 -0500

    Update product.py

--- l10n_mx_edi/models/product.py
+++ l10n_mx_edi/models/product.py
@@ -7,7 +7,7 @@ class ProductTemplate(models.Model):
 
     unidad_medida_id = fields.Many2one('unidad.medida', string='Unidad SAT')
     clave_producto_id = fields.Many2one('producto.servicio', string='Clave Producto/Servicio SAT')
-    objetoimp = fields.Selection(
+    objeto_imp = fields.Selection(
         selection=[('01', 'No objeto de impuesto'), 
                    ('02', 'Sí objeto de impuesto'), 
                    ('03', 'Sí objeto del impuesto y no obligado al desglose'),],
